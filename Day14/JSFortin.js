86798563b74d23b7a3389b028314c64e83ee47ab
BableshAAzad <bableshaazad@outlook.com>
2023-11-03 12:40:39 +0530

    Higher order function

---
+++ Day14/JSFortin.js
@@ -0,0 +1,28 @@
+
+// let x = parseInt(prompt("Enter first number : "));
+// let y = parseInt(prompt("Enter Second number : "));
+
+// abc = (x, y, xyz) => {
+//     xyz(x, y);
+// };
+// abc(x, y, (a, b) => {
+//     console.log(a + b);
+// });
+
+// abc(x, y, (a, b) => {
+//     console.log(a * b);
+// });
+
+// abc(x, y, (a, b) => {
+//     console.log(a - b);
+// });
+
+// console.log(`The addition of ${x} and ${y} is : 
+// ${abc (x, y, function(a, b){ 
+//     return a+b;
+// })}`);
+
+// !----------------------------------------------------
+let l = parseInt(prompt("Enter the length : "));
+let r = parseInt(prompt("Enter the radius : "));
+let b = parseInt(prompt("Enter the breadth : "));
