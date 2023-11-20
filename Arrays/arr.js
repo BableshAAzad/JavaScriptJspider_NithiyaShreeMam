8a3240ac7ba531837d6cd74c89b5a1125239d491
BableshAAzad <bableshaazad@outlook.com>
2023-11-20 23:51:19 +0530

    arrays methods js

--- Arrays/arr.js
+++ Arrays/arr.js
@@ -96,18 +96,18 @@
 // ? join() converts the array to string 
 // ? the argument inside join method will be added between each element of an array
 // ?doesn't effects the original array
-let arr = [10, 20, 30, 40, 50];
-let e = arr.join(); // console.log(arr.join());
-console.log(e); // 10,20,30,40,50
-console.log(arr.join(""))// 1020304050
-console.log(arr.join(" ")); // 10 20 30 40 50
-console.log(arr.join("+")); // 10+20+30+40+50
-let a = arr.join();
-console.log(typeof a); // string
-console.log(arr); // [10, 20, 30, 40, 50]
+// let arr = [10, 20, 30, 40, 50];
+// let e = arr.join(); // console.log(arr.join());
+// console.log(e); // 10,20,30,40,50
+// console.log(arr.join(""))// 1020304050
+// console.log(arr.join(" ")); // 10 20 30 40 50
+// console.log(arr.join("+")); // 10+20+30+40+50
+// let a = arr.join();
+// console.log(typeof a); // string
+// console.log(arr); // [10, 20, 30, 40, 50]
 // ^------------------------------------------------------------------------------
 // ! fill()
-// ? fil() every elemnt of an array with the passed argument
+// ? fill() every element of an array with the passed argument
 // ? if no argument is passed then it is consider an undefined
 // ? effects the original array
 // let arr = [10, 20, 30, 40, 50];
@@ -116,19 +116,23 @@ console.log(arr); // [10, 20, 30, 40, 50]
 // let brr  = [10, 20, 30, 40, 50];
 // brr.fill(11);
 // console.log(brr); // [11, 11, 11, 11, 11]
+// let w = [10, 20, 30, 40, 50];
+// console.log(w.fill(24, 45)); // [10, 20, 30, 40, 50]
 // ^------------------------------------------------------------------------------
 // ! includes(search_element, from_index)
-// ? include() search whether the elememt is present or not, if the element present return true
+// ? includes() search whether the elememt is present or not, if the element present return true
 // ? otherwise false where the result in the from of boolean
 // ? accepts the search _element and from_index to the search of an element from the given index in forwords
 // ? if from_index is not specified by default searchs from 0 index
 // let arr = [10, 20, 30, 40, 50];
-//  console.log(arr.includes()); // false
-//  console.log(arr.includes(30)); // true
-//  console.log(arr.includes("30")); // false
-//  console.log(arr.includes(30, 3)); // false
-//  console.log(arr.includes(30, 2)); // true
-//  console.log(arr.includes(30, -1)); // false
+// console.log(arr.includes()); // false
+// let y = arr.includes(30);
+// console.log(y); // true
+// console.log(arr.includes(30)); // true
+// console.log(arr.includes("30")); // false
+// console.log(arr.includes(30, 3)); // false
+// console.log(arr.includes(30, 2)); // true
+// console.log(arr.includes(30, -1)); // false
 // ^------------------------------------------------------------------------------
 // ! indexOf(search_element, from_index)
 // ? indexOf() returns the index value of the element
@@ -136,7 +140,7 @@ console.log(arr); // [10, 20, 30, 40, 50]
 // ?  indexOf()  retuns the index of first matched element
 // ? if from_index is declared of the indexOf()  search the element from the given
 // ? index value in forward direction othewise search the element from the 0 index in frorword direction
-// let arr = [10, 20, 30, 40, 50];
+// let arr = [10, 20, 30, 40, 50, 30];
 // console.log(arr.indexOf()); // -1
 // console.log(arr.indexOf(30)); // 2
 // console.log(arr.indexOf(60)); // -1
@@ -145,32 +149,36 @@ console.log(arr); // [10, 20, 30, 40, 50]
 // console.log(arr.indexOf(30, -2)); // 5
 // console.log(arr.indexOf(30, -4)); // 2
 // ^------------------------------------------------------------------------------
-// ! lasetIndexOf(search_element, from_index)
-// ? lasetIndexOf() returns the index value of the element
+// ! lastIndexOf(search_element, from_index)
+// ? lastIndexOf() returns the index value of the element
 // ?  if the elements is not present retuns -1
-// ? lasetIndexOf() retuns the index of last matched element becase the search is in backword direction
-// ? from_index is declared the lasetIndexOf() search the element from the given index value in backward
-// ? direction otherwise search the element from the backword direction (from the laset index to 0 index).
-// let arr = [10, 20, 30, 40, 50];
+// ? lastIndexOf() retuns the index of last matched element becase the search is in backword direction
+// ? if from_index is declared the lastIndexOf() search the element from the given index value in backward
+//?direction otherwise search the element from the backword direction (from the last index to 0 index).
+// let arr = [10, 20, 30, 40, 50, 30];
 // console.log(arr.lastIndexOf()); // -1
 // console.log(arr.lastIndexOf(30)); // 5
 // console.log(arr.lastIndexOf("30")); // -1
-// console.log(arr.lastIndexOf(80)); //
-// console.log(arr.lastIndexOf());
-// console.log(arr.lastIndexOf());
-// console.log(arr.lastIndexOf());
+// console.log(arr.lastIndexOf(80)); // -1
+// console.log(arr.lastIndexOf(30, 1)); // -1
+// console.log(arr.lastIndexOf(30, -3)); // 2
+// console.log(arr.lastIndexOf(30, -1)); // 5
 // ^------------------------------------------------------------------------------
 // ! flat()
 // ?  flat() flattens the nested array
 // ? the argument can be passed depending upon the number of nested array or else Infinite can be specified
-// let arr = [10, 20, 30, 40, 50];
-// console.log(arr); // [10, 20, 30, 40, 50]
-// console.log(arr.flat()); // [10, 20, 30, 40, 50]
-// console.log(arr.flat(1)); // [10, 20, 30, 40, 50]
+let arr = [10, 20, 30, 40, 50];
+console.log(arr); // [10, 20, 30, 40, 50]
+console.log(arr.flat()); // [10, 20, 30, 40, 50]
+console.log(arr.flat(1)); // [10, 20, 30, 40, 50]
 
-// let brr = [10, 20, 30, 40, 50];
-// console.log(brr); // [10, 20, 30, 40, 50]
+let brr = [10, 20, [[30]], 40, 50];
+console.log(brr); // [10, 20, Array(1), 40, 50]
+console.log(brr.flat(2)); // [10, 20, 30, 40, 50]
 
+let crr = [10,20, [[[[[[[[[30]]]]]]]]], 40, 50];
+console.log(crr); // [10, 20, Array(1), 40, 50]
+console.log(crr.flat(Infinity)); // [10, 20, 30, 40, 50]
 
 // ^------------------------------------------------------------------------------
 // ! reverse()
