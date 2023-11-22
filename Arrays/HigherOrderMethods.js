dc9f85995c6d610c34f2b3e593d65058820e988a
BableshAAzad <bableshaazad@outlook.com>
2023-11-22 22:02:40 +0530

    demo

--- Arrays/HigherOrderMethods.js
+++ Arrays/HigherOrderMethods.js
@@ -1,6 +1,11 @@
 // ^ Array Higher Order Methods
+// ? the method which accepts the callback function is called higher order function
 // ! map()
-let arr = [10, 20, 30, 40, 50];
+// ? The callback function of map() accepts three arguments that is element, index, 
+// ?
+// ?
+
+// let arr = [10, 20, 30, 40, 50];
 // arr.map((ele, index, array) => {
 //     console.log(ele, index, array);
 // }); // [10, 20, 30, 40, 50] print 5 times to array
@@ -28,11 +33,26 @@ let arr = [10, 20, 30, 40, 50];
 // ? Take Array of elements filter the elements which are divisible by 5,
 //? then multiply every filtered array elemenets whith 2
 
-let brr = [10, 16, 19, 25, 36, 27, 35];
-let result = brr.filter((ele) => {
-    if (ele % 5 == 0)
-        return ele; // [10, 25, 35]
-}).map((abc) => {
-    return abc * 2;  // [20, 50, 70]
+// let brr = [10, 16, 19, 25, 36, 27, 35];
+// let result = brr.filter((ele) => {
+//     if (ele % 5 == 0)
+//         return ele; // [10, 25, 35]
+// }).map((abc) => {
+//     return abc * 2;  // [20, 50, 70]
+// });
+// console.log(result);
+// ^---------------------------------------------------------------------
+// ! reduce()
+let arb = [10, 20, 30, 40, 50];
+let res = arb.reduce((accu, value) => {
+    console.log(accu);
+    return accu + value;
 });
-console.log(result);
+console.log("++++++++++++++")
+console.log(res);
+// ^---------------------------------------------------------------------
+
+let acc = [10, 20, 30, 40, 50];
+let bcc = ['a', 'b', 'c'];
+let ccc = [true, false];
+console.log(acc.concat(bcc, 94, 100, ccc))
