cf2d1a53ed8880d52b660123fde41aa03f5575e6
BableshAAzad <bableshaazad@outlook.com>
2023-12-12 00:31:21 +0530

    local and sesion practice js

--- LocalSessionStorage/localandSessionStrorage.js
+++ LocalSessionStorage/localandSessionStrorage.js
@@ -1,36 +1,42 @@
 // ! Local Storage
-// console.log(localStorage);
-// console.log(typeof localStorage);
+console.log(localStorage);
+console.log(typeof localStorage);
 // & store
-// localStorage.setItem("username", "Bablesh");
-// localStorage.setItem("username", "aazad");
+localStorage.setItem("username", "Bablesh");
+localStorage.setItem("username", "AAzad");
 
-// localStorage.setItem("password", 12345);
+localStorage.setItem("password", 12345);
 // & fetch
-// console.log(localStorage.getItem("username"));
-// console.log(localStorage.getItem("password"));
+console.log(localStorage.getItem("username"));
+console.log(localStorage.getItem("password"));
 // & remove
 // localStorage.removeItem("username");
 // & clear
 // localStorage.clear();
 // console.log(localStorage)
-// console.log(localStorage.key("Bablesh"));
-// console.log(localStorage.key(0));
-// console.log(localStorage.key(12345));
+// & key
+console.log(localStorage.key(0));
+console.log(localStorage.key(1));
+// & length
+console.log(localStorage.length);
 
 // ^-----------------------------------------------
 // ! session Storage
-// console.log(sessionStorage);
-// console.log(typeof sessionStorage);
+console.log(sessionStorage);
+console.log(typeof sessionStorage);
 // & store
-// sessionStorage.setItem("username", "Bablesh");
-// sessionStorage.setItem("password", 12345);
+sessionStorage.setItem("username", "Bablesh");
+sessionStorage.setItem("password", 12345);
 // & fetch
-// console.log(sessionStorage.getItem("username"));
-// console.log(sessionStorage.getItem("password"));
+console.log(sessionStorage.getItem("username"));
+console.log(sessionStorage.getItem("password"));
 // & remove
 // sessionStorage.removeItem("username");
 // & clear
 // sessionStorage.clear();
-
+// & key
+console.log(sessionStorage.key(0));
+console.log(sessionStorage.key(1));
+// & length
+console.log(sessionStorage.length);
 
