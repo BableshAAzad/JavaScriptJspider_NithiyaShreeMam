bbcc8748857db0766215d099c7c050c54f1f8497
BableshAAzad <bableshaazad@outlook.com>
2023-12-16 00:00:04 +0530

    kk

--- AsyncAndAwait/Cards.js
+++ AsyncAndAwait/Cards.js
@@ -1,5 +1,11 @@
-
-function show() {
-    // document.getElementById("secID").innerHTML = "Bablesh AAzad";
-    fetch("Data.json").then()
-}
+fetch("./Data.json").then((msg) => {
+    msg.json().then((msg) => {
+        msg.map((ele) => {
+            // console.log(ele.ename);
+            document.getElementById("secID").innerHTML = `
+            <div>
+            <img src=${ele.epic}></img>
+            </div>`;
+        })
+    })
+})
