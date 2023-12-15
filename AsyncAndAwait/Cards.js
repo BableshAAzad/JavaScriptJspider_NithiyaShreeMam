d107a5b866e4ead42933a3d0412b8e9938d80706
BableshAAzad <bableshaazad@outlook.com>
2023-12-16 00:19:06 +0530

    json fetch data

--- AsyncAndAwait/Cards.js
+++ AsyncAndAwait/Cards.js
@@ -1,11 +1,26 @@
-fetch("./Data.json").then((msg) => {
-    msg.json().then((msg) => {
-        msg.map((ele) => {
-            // console.log(ele.ename);
-            document.getElementById("secID").innerHTML = `
-            <div>
-            <img src=${ele.epic}></img>
-            </div>`;
-        })
+// fetch("./Data.json").then((msg) => {
+//     msg.json().then((msg) => {
+//         msg.map((ele) => {
+//             console.log(ele.ename);
+//             document.getElementById("secID").innerHTML = `
+//             <div>
+//             <img src=${ele.epic}></img>
+//             </div>`;
+//         })
+//     }).catch((err)=>{
+//         err("hi");
+//     })
+// });
+
+let diveR = document.getElementById("secID");
+async function eDetails() {
+    let data = await fetch("./Data.json");
+    let finalData = await data.json();
+    finalData.map((ele) => {
+        diveR.innerHTML = `<div> 
+        <img src=${ele.epic}></img>
+        </div>`;
+        // console.log(ele);
     })
-})
+}
+eDetails();
