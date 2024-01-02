1166cec73e19f13d3ca2b124171b2789a78d3e3a
BableshAAzad <bableshaazad@outlook.com>
2024-01-02 23:31:13 +0530

    progragramming class practice

---
+++ ProgrammingClass/ClassSecond.js
@@ -0,0 +1,11 @@
+let num = Number(prompt("Enter year : "));
+// if (num % 4 == 0 && num % 100 != 0 || num % 400 == 0)
+//     console.log(`${num} is a leap year`);
+// else
+//     console.log(`${num} in not a leap year`);
+
+
+if (num % 5 == 0)
+    console.log(`${num} is divisible by 5`);
+else
+    console.log(`${num} is not divisible by 5`)
