3f32d3b9bebf38e8a8f8a28e2e9e6fbcb0765804
BableshAAzad <bableshaazad@outlook.com>
2024-01-03 22:18:54 +0530

    demo home page design part 1

---
+++ PracticeFaltu/DeskTopPage/PageHome.js
@@ -0,0 +1,37 @@
+function leftSideDiv11() {
+    let temp = document.querySelector("#bodydiv1");
+    temp.scrollIntoView({ behavior: "smooth" });
+}
+function leftSideDiv12() {
+    let temp = document.querySelector("#bodydiv2");
+    temp.scrollIntoView({ behavior: "smooth" });
+}
+function leftSideDiv13() {
+    let temp = document.querySelector("#bodydiv3");
+    temp.scrollIntoView({ behavior: "smooth" });
+}
+function leftSideDiv14() {
+    let temp = document.querySelector("#bodydiv4");
+    temp.scrollIntoView({ behavior: "smooth" });
+}
+function leftSideDiv15() {
+    let temp = document.querySelector("#bodydiv5");
+    temp.scrollIntoView({ behavior: "smooth" });
+}
+function leftSideDiv16() {
+    let temp = document.querySelector("#bodydiv6");
+    temp.scrollIntoView({ behavior: "smooth" });
+}
+function leftSideDiv17() {
+    let temp = document.querySelector("#bodydiv7");
+    temp.scrollIntoView({ behavior: "smooth" });
+}
+function leftSideDiv18() {
+    let temp = document.querySelector("#bodydiv8");
+    temp.scrollIntoView({ behavior: "smooth" });
+}
+
+document.getElementById("buttonTop").onclick = function () {
+    document.getElementById("navbar").scrollIntoView({ behavior: "smooth" });
+}
+
--- ProgrammingClass/ClassSecond.js
+++ ProgrammingClass/ClassSecond.js
@@ -1,11 +1,26 @@
-let num = Number(prompt("Enter year : "));
+// let num = Number(prompt("Enter year : "));
 // if (num % 4 == 0 && num % 100 != 0 || num % 400 == 0)
 //     console.log(`${num} is a leap year`);
 // else
 //     console.log(`${num} in not a leap year`);
 
 
-if (num % 5 == 0)
-    console.log(`${num} is divisible by 5`);
-else
-    console.log(`${num} is not divisible by 5`)
+// if (num % 5 == 0)
+//     console.log(`${num} is divisible by 5`);
+// else
+//     console.log(`${num} is not divisible by 5`)
+
+
+// let a = Number(prompt("Enter number a : "));
+// let a = 16;
+// let temp = Math.sqrt(a);
+// let res = Math.ceil(temp);
+// console.log(temp);
+// console.log(res)
+// let b = (temp == res) ? "perfect" : "not a perfect";
+// console.log(b);
+
+let temp = "MOM";
+let res = temp.split("").reverse().join("");
+let result = temp == res ? "palindrome" : " Not plalindrome";
+console.log(result)
