913594774fb8e2c01a81969c5f9305fd084c5202
BableshAAzad <bableshaazad@outlook.com>
2024-01-20 21:16:59 +0530

    object practice

--- ObjectJS/CreateMethod.js
+++ ObjectJS/CreateMethod.js
@@ -1,6 +1,15 @@
-let obj = Object.create({
-    name: "Bablesh",
-    add: "Raipur",
-});
-Object.getPrototypeOf(obj).name = "AAzad";
-console.log(Object.getPrototypeOf(obj).name);
+// let obj = Object.create({
+//     name: "Bablesh",
+//     add: "Raipur",
+// });
+// Object.getPrototypeOf(obj).name = "AAzad";
+// console.log(Object.getPrototypeOf(obj).name);
+
+let obje = {
+    ename: "AAzad",
+    age: 17,
+    address: "Raipur"
+}
+for (let i of Object.entries(obje)) {
+    console.log(i)
+}
