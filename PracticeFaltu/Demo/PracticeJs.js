a5e4077ab3bb96c33910bbb926668afd15b81164
BableshAAzad <bableshaazad@outlook.com>
2024-02-12 16:41:59 +0530

    practice

---
+++ PracticeFaltu/Demo/PracticeJs.js
@@ -0,0 +1,18 @@
+"use strict"; //^ Use Strict mode
+// a = 10;
+// alert(a + " Hello");
+// var a;
+// console.log(a)
+// *-------------------------------------------
+// function let(){
+//     console.log("hello")
+// }
+// let();
+// *-------------------------------------------
+// var a = 20;
+// for (var i = 0; i < 5; i++) {
+//     console.log(i, a)
+// }
+// *-------------------------------------------
+// const newSet = new Set([4, 5, 6, 7]);
+// console.log(newSet);// Outputs Set {4,5,6,7}
